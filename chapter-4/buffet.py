fb245e6ee14b2198a8539d72eddffbf2f64f66f1
Kyle B. Lackinger <karl.B.HODL@proton.me>
2024-02-15 22:20:15 -0500

    finished chapter 4 exercises

---
+++ chapter-4/buffet.py
@@ -0,0 +1,11 @@
+foods = ('pizza', 'hamburger', 'tuna', 'tacos', 'sushi')
+print("Original menu:")
+for food in foods:
+    print(food)
+
+# foods[0] = 'carrot cake' # this will raise an error
+    
+foods = ('carrot cake', 'hamburger', 'tuna', 'tacos', 'salad')
+print("\nModified menu:")
+for food in foods:
+    print(food)
---
+++ chapter-4/dimensions.py
@@ -0,0 +1,12 @@
+dimensions = (200, 50)
+print(dimensions[0])
+print(dimensions[1])
+
+print("Original dimensions:")
+for dimension in dimensions:
+    print(dimension)
+
+print("\nModified dimensions:")
+dimensions = (400, 100)
+for dimension in dimensions:
+    print(dimension)
