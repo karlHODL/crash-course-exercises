27d584a164a1dd1be70e23ac9e75098583594257
Kyle B. Lackinger <karl.B.HODL@proton.me>
2024-02-21 22:31:09 -0500

    started chapter 5

---
+++ chapter-5/conditional_tests.py
@@ -0,0 +1,42 @@
+topping = 'banana peppers'
+print('Is topping == "banana peppers" I predict True.')
+print(topping == 'banana peppers')
+
+print('\nIs topping == "anchovies" I predict False.')
+print(topping == 'anchovies')
+
+requested_toppings = ['mushrooms', 'onions', 'pineapple']
+print('\nIs "mushrooms" in requested_toppings? I predict True.')
+print('mushrooms' in requested_toppings)
+
+print('\nIs "pepperoni" in requested_toppings? I predict False.')
+print('pepperoni' in requested_toppings)
+
+print('\nIs "anchovies" not in requested_toppings? I predict True.')
+print('anchovies' not in requested_toppings)
+
+print('\nIs "mushrooms" not in requested_toppings? I predict False.')
+print('mushrooms' not in requested_toppings)
+
+age = 18
+print('\nIs age == 18? I predict True.')
+print(age == 18)
+
+print('\nIs age != 18? I predict False.')
+print(age != 18)
+
+print('\nIs age > 18? I predict False.')
+print(age > 18)
+
+print('\nIs age <= 18? I predict True.')
+print(age <= 18)
+
+age_0 = 22
+age_1 = 20
+
+print('\nIs age_0 > 21 and age_1 > 21? I predict False.')
+print(age_0 > 21 and age_1 > 21)
+
+car = "Audi"
+print('\nIs car.lower() == "audi"? I predict True.')
+print(car.lower() == "audi")
---
+++ chapter-5/toppings.py
@@ -0,0 +1,7 @@
+topping = 'banana peppers'
+if topping != 'anchovies':
+    print("Hold the anchovies!")
+
+requested_toppings = ['mushrooms', 'onions', 'pineapple']
+print('mushrooms' in requested_toppings)
+print('pepperoni' in requested_toppings)
