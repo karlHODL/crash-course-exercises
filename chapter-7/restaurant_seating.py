8f6c9be9f578565dfb9d7877146bc9254270dc49
Kyle B. Lackinger <karl.B.HODL@proton.me>
2024-03-08 22:15:46 -0500

    started chapter 7 exercises

---
+++ chapter-7/restaurant_seating.py
@@ -0,0 +1,6 @@
+party_size = int(input("How many people are in your dinner group? "))
+
+if party_size > 8:
+    print("I'm sorry, you'll have to wait for a table.")
+else:
+    print("Your table is ready.")
---
+++ chapter-7/rollercoaster.py
@@ -0,0 +1,5 @@
+height = int(input("How tall are you, in inches? "))
+if height >= 48:
+    print("\nYou're tall enough to ride!")
+else:
+    print("\nYou'll be able to ride when you're a little taller.")
