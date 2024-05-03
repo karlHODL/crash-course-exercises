39f808960ba6e8d3cca216cda1543a628ee3c9ba
Kyle B. Lackinger <karl.B.HODL@proton.me>
2024-05-02 21:47:11 -0400

    finished chapter 12 portion of alien invasion

--- chapter_12/alien_invasion/alien_invasion.py
+++ chapter_12/alien_invasion/alien_invasion.py
@@ -28,7 +28,7 @@ class AlienInvasion:
         while True:
             self._check_events()
             self.ship.update()
-            self.bullets.update()
+            self._update_bullets()
             self._update_screen()
             self.clock.tick(60)
 
@@ -50,6 +50,8 @@ class AlienInvasion:
             self.ship.moving_left = True
         elif event.key == pygame.K_q:
             sys.exit()
+        elif event.key == pygame.K_SPACE:
+            self._fire_bullet()
 
     def _check_keyup_events(self, event):
         """Respond to key releases."""
@@ -58,9 +60,26 @@ class AlienInvasion:
         elif event.key == pygame.K_LEFT:
             self.ship.moving_left = False
 
+    def _fire_bullet(self):
+        """Create a new bullet and add it to the bullets group."""
+        if len(self.bullets) < self.settings.bullets_allowed:
+            new_bullet = Bullet(self)
+            self.bullets.add(new_bullet)
+    
+    def _update_bullets(self):
+        """Update position of bullets and get rid of old bullets."""
+        self.bullets.update()
+
+        # Get rid of the bullets that have disappeared.
+        for bullet in self.bullets.copy():
+            if bullet.rect.bottom <= 0:
+                self.bullets.remove(bullet)
+    
     def _update_screen(self):
         """Update images on the screen, and flip to the new screen."""
         self.screen.fill(self.settings.bg_color)
+        for bullet in self.bullets.sprites():
+            bullet.draw_bullet()
         self.ship.blitme()
             
         # Make the most recently drawn screen visible.
--- chapter_12/alien_invasion/settings.py
+++ chapter_12/alien_invasion/settings.py
@@ -13,5 +13,6 @@ class Settings:
         # Bullet settings
         self.bullet_speed = 2.0
         self.bullet_width = 3
-        self.bullet_height = 15
+        self.bullet_height = 12
         self.bullet_color = (60, 60, 60)
+        self.bullets_allowed = 3
