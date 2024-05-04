f6262ae2653754e2a4cb094f127e9ee4cd8e6893
Kyle B. Lackinger <karl.B.HODL@proton.me>
2024-05-04 19:40:51 -0400

    finished sideways shooter exercise

--- chapter_12/sideways_shooter/settings.py
+++ chapter_12/sideways_shooter/settings.py
@@ -8,11 +8,11 @@ class Settings:
         self.bg_color = (230, 230, 230)
 
         # Ship settings
-        self.ship_speed = 1.5
+        self.ship_speed = 2.5
 
         # Bullet settings
-        self.bullet_speed = 2.0
-        self.bullet_width = 3
-        self.bullet_height = 12
+        self.bullet_speed = 4.0
+        self.bullet_width = 12
+        self.bullet_height = 3
         self.bullet_color = (60, 60, 60)
-        self.bullets_allowed = 3
+        self.bullets_allowed = 6
